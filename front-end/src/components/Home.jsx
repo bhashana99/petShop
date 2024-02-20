70f3930e2854ce619ae6dea9fe0ae1b68da7e55b
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-02-20 10:46:16 +0530

    add image slider

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function Home() {
   return (
-    <div className='bg-red-700'>Home</div>
-  )
+    <div className="h-[400px] md:h-[700px] transition-transform transform origin-center">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        
+      </Swiper>
+    </div>
+  );
 }
