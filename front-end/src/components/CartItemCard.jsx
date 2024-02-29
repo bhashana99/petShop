2afaacf677513bf307cd04d43434a2c759e69b39
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-02-29 12:49:23 +0530

    added trail data for css

--- front-end/src/components/CartItemCard.jsx
+++ front-end/src/components/CartItemCard.jsx
@@ -2,23 +2,29 @@ import "../styles/cart.css";
 import React from "react";
 
 function CartItemCard() {
-  return ( 
-  <div className="card-bg">
-    <div className="cart-img"></div>
-    <div className="cart-details">
-        <div className="item-name"></div>
-        <div className="item-price"></div>
-        <div className="item-QtyButton">
-          <button>+</button>
-          <button>2</button>
-          <button>-</button>
+  return (
+    <div className="card-box">
+      <div className="cart-img">
+        <img
+          src="https://m.media-amazon.com/images/I/810OOg88LoL._AC_UY327_FMwebp_QL65_.jpg"
+          alt="product-image"
+        />
+      </div>
+      <div className="cart-details">
+        <div className="item-name">"My First Book of Pencil Control"</div>
+        <div className="item-price">Rs.89/=</div>
+        <div className="item-button">
+          <div className="item-QtyButton">
+            <button>+</button>
+            <button>2</button>
+            <button>-</button>
+          </div>
+          <div className="item-remove">
+            <button>remove</button>
+          </div>
         </div>
-        <div className="item-remove">
-          <button>remove</button>
-        </div>
-
+      </div>
     </div>
-  </div>
   );
 }
 
