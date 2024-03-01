de5cfd2ead211d1639705ea8321fa8cc4e763169
peheliyanavod <hwpeheliya@gmail.com>
2024-03-01 13:52:47 +0530

    Created a component to view all products

---
+++ front-end/src/components/ViewAll.jsx
@@ -0,0 +1,18 @@
+function ViewAll() {
+
+    return ( 
+
+        <div>
+            <h1 className='heading'>Products</h1>
+            <div>
+                
+            </div>
+            <div className='product-div'>
+            
+                
+            </div>
+        </div>
+     );
+}
+
+export default ViewAll;
