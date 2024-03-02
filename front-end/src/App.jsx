0a96a78a476608abca1d1359001281c2de74edab
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-03-03 02:11:54 +0530

    All conflicts resolved

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,31 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
+import Footer from './components/Footer'
 import Home from './components/Home'
-import Nav from "./components/Nav";
-// import Cart from "./pages/Cart";
-// import { useState } from "react";
-// import CartItemCard from "./components/CartItemCard";
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Nav from './components/Nav'
+import SingleProductDetails from "./components/SingleProductDetails";
+import Shop from "./components/Shop";
 
 function App() {
   
-  // const [cart,setCart] = useState([]);
 
   return (
-
     <BrowserRouter>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-      {/* <Nav />
-      <Routes>
-        <Route path="/cart" element={<Cart/>} />
-      </Routes>
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/product-details/:id" element={<SingleProductDetails/>} />
+        <Route path="/shop" element={<Shop/>} />
 
-      <Footer /> */}
+      </Routes>
+      
+   
+      <Footer />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
