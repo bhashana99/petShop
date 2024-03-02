08000827caf27a5490e8e88268b259fba50561dc
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-03-03 01:38:38 +0530

    resolve conflicts

--- back-end/src/main/java/com/example/backend/BackEndApplication.java
+++ back-end/src/main/java/com/example/backend/BackEndApplication.java
@@ -3,8 +3,10 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.example.backend", "com.example.backend.service"})
 public class BackEndApplication {
 
     public static void main(String[] args) {
