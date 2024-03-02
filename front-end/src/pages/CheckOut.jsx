150f4180279f6f2c6e136217d67eefe5a0acd5c2
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-03-03 03:13:22 +0530

    Basic structure of checkout completed

--- front-end/src/pages/CheckOut.jsx
+++ front-end/src/pages/CheckOut.jsx
@@ -1,6 +1,49 @@
 //import React from 'react'
+import axios from "axios";
+import {useEffect,useState,} from "react";
 
 export default function CheckOut (){
+
+    const [users,setUsers] = useState([]);
+    const [firstName,setFirstName] =useState("");
+    const [lastName,setLastName] =useState("");
+    const [phoneNumber,setPhoneNumber] =useState("");
+    const [email,setEmail] =useState("");
+    const [address,setAddress] =useState("");
+    const [city,setCity] =useState("");
+    const [state,setState] =useState("");
+    const [zipCode,setZipCode] =useState("");
+
+async function save(event){
+    event.preventDefault();
+    try{
+        await axios.post("http://localhost:8090/Order",{
+        firstName:firstName,
+        lastName:lastName,
+        phoneNumber:phoneNumber,
+        email:email,
+        address:address,
+        city:city,
+        state:state,
+        zipCode:zipCode,
+        });
+        alert("User details submitted successfully!");
+    }catch(err){
+        alert("Error submitting user details. Please try again.")
+    }
+}
+
+
+    useEffect(()=>{
+        loadOrders();
+    },[]);
+     
+    const loadOrders=async () =>{
+        const result =await axios.post("http://localhost:8080/Order");
+        setUsers(result.data);
+        console.log(result.data);
+    }
+
   return (
     <div className='checkout'>
           <div className="py-3 bg-warning">
@@ -49,54 +92,95 @@ export default function CheckOut (){
                                     <div className='col-md-6'>
                                         <div className='from-group md-3'>
                                             <label>Frist  Name</label>
-                                            <input type='text' name='fristName' className='form-control'/>
+                                            <input placeholder="Enter Frist Name" type='text' id="fristname1" className='form-control'
+
+                                            value={firstName}
+
+                                            onChange={(event)=>{
+                                                setFirstName(event.target.value)
+                                            }}/>
                                         </div>
                                     </div>
                                     <div className='col-md-5'>
                                         <div className='from-group md-3'>
                                             <label>Last Name</label>
-                                            <input type='text' name='lastName' className='form-control'/>
+                                            <input placeholder="Enter Last Name" type='text' id='lastname1' className='form-control'
+                                            value={lastName}
+                                            onChange={(event)=>{
+                                                setLastName(event.target.value)
+                                            }}
+                                            />
                                         </div>
                                     </div>
                                     <div className='col-md-5'>
                                         <div className='from-group md-3'>
                                             <label>phone Number</label>
-                                            <input type='text' name='phoneNumber' className='form-control'/>
+                                            <input placeholder="Enter Phone Number" type='text' id='phonenumber1' className='form-control'
+                                            value={phoneNumber}
+                                            onChange={(event)=>{
+                                                setPhoneNumber(event.target.value)
+                                            }}
+                                            />
                                         </div>
                                     </div>
                                     <div className='col-md-6'>
                                         <div className='from-group md-3'>
                                             <label>email</label>
-                                            <input type='email' name='email' className='form-control'/>
+                                            <input placeholder="Enter email" type='email' id='email1' className='form-control'
+                                            value={email}
+                                            onChange={(event)=>{
+                                                setEmail(event.target.value)
+                                            }}
+                                            />
                                         </div>
                                     </div>
                                     <div className='col-md-11'>
                                         <div className='from-group md-3'>
                                             <label>Full Address</label>
-                                            <textarea  rows="3" className='form-control'></textarea>
+                                            <textarea placeholder="Enter Address" rows="3" id='addRess1' className='form-control'
+                                            value={address}
+                                            onChange={(event)=>{
+                                                setAddress(event.target.value)
+                                            }}
+                                            />
                                         </div>
                                     </div>
                                     <div className='col-md-4'>
                                         <div className='from-group md-3'>
                                             <label>City</label>
-                                            <input type='text' name='city' className='form-control'/>
+                                            <input placeholder="Enter City" type='text' id='city1' className='form-control'
+                                            value={city}
+                                            onChange={(event)=>{
+                                                setCity(event.target.value)
+                                            }}
+                                            />
                                         </div>
                                     </div>
                                     <div className='col-md-4'>
                                         <div className='from-group md-3'>
                                             <label>State</label>
-                                            <input type='text' name='state' className='form-control'/>
+                                            <input placeholder="Enter State" type='text' id='state1' className='form-control'
+                                            value={state}
+                                            onChange={(event)=>{
+                                                setState(event.target.value)
+                                            }}
+                                            />
                                         </div>
                                     </div>
                                      <div className='col-md-3'>
                                         <div className='from-group md-3'>
                                             <label>Zip code</label>
-                                            <input type='text' name='zipCode' className='form-control'/>
+                                            <input placeholder="ZipCode" type='text' id='zipcode1' className='form-control'
+                                            value={zipCode}
+                                            onChange={(event)=>{
+                                                setZipCode(event.target.value)
+                                            }}
+                                            />
                                         </div>
                                     </div>
                                     <div className='col-md-12'>
                                         <div className='from-group text-end'>
-                                        <button type='button' className='btn btn-primary'>Place Order</button>
+                                        <button type='submit' className='btn btn-primary' onClick={save}>Place Order</button>
                                         </div>
                                     </div>
 
