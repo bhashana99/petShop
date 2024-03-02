fdabdf447a05f9e9eee64c62681c183b2915611e
peheliyanavod <hwpeheliya@gmail.com>
2024-03-03 04:14:19 +0530

    Some missed files commited

--- front-end/src/components/SingleProductDetails.jsx
+++ front-end/src/components/SingleProductDetails.jsx
@@ -1,23 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 function SingleProductDetails() {
-    return (
-      <div className="items-center">
-          <div className="single-product-container">
-            <div className="productImage">
-              <img src="images/cat food 1.jpg" alt="" />
-            </div>
-            <div className="productDetails">
-              <p className="text-2xl p-1">Cat food</p>
-              <p className="text-lg p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eos ipsam ipsum cumque eligendi, 
-                nostrum ipsa corrupti illo doloremque doloribus minima modi ea maiores facere sit itaque, 
-                aliquid nisi eaque.</p>
-              <p className="text-xl p-4">Rs.2000/=</p>
-              
-              <button className="btn">Add to cart</button>
-            </div>
-        </div>
-      </div>
-        
-      );
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    loadProduct();
+  });
+
+  const loadProduct = async () => {
+    const result = await axios.get(`http://localhost:8010/petshop/getProductById?id=`+id);
+    setProduct(result.data);
+    console.log(product.image);
+  };
+
+  return (
+    <div>
+      
+              <div className="items-center" >
+                <div className="single-product-container">
+                  <div className="productImage">
+                  <img src={`../images/` + product.image} alt="" />
+                  </div>
+                  <div className="productDetails">
+                    <p className="text-2xl p-1">{product.name}</p>
+                    <p className="text-lg p-3">{product.description}</p>
+                    <p className="text-xl p-4">Rs.{product.price}/=</p>
+                    <button className="btn">Add to cart</button>
+                  </div>
+                </div>
+              </div>          
+  
+    </div>
+  );
 }
 
 export default SingleProductDetails;
