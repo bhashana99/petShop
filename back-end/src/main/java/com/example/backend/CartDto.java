6fcd32b3c9c0f169e406c783fad964ba8ea41996
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-03-03 15:54:52 +0530

    CartDto code added

--- back-end/src/main/java/com/example/backend/CartDto.java
+++ back-end/src/main/java/com/example/backend/CartDto.java
@@ -1,5 +1,30 @@
 package com.example.backend;
 
-public class cartDto {
+import org.springframework.web.bind.annotation.RequestBody;
 
+public class CartDto {
+    private int productId;
+    private int quantity;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public CartDto(@RequestBody CartDto CartDto){
+        this.productId=productId;
+        this.quantity= quantity;
+
+    }
 }
