2199c20dc3b73c20994a296f23038a3dad5a2c41
peheliyanavod <hwpeheliya@gmail.com>
2024-03-03 16:26:29 +0530

    simple changes in app.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer'
 import Home from './components/Home'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
+// import SignUp from './pages/SignUp'
+// import SignIn from './pages/SignIn'
 import Nav from './components/Nav'
 import SingleProductDetails from "./components/SingleProductDetails";
 import Shop from "./components/Shop";
+import Cart from "./pages/Cart";
 
 function App() {
   
@@ -15,10 +16,11 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
+        {/* <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/sign-in" element={<SignIn/>} /> */}
         <Route path="/product-details/:id" element={<SingleProductDetails/>} />
         <Route path="/shop" element={<Shop/>} />
+        <Route path="/cart" element={<Cart/>} />
 
       </Routes>
       
